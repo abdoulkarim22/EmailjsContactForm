aa1b95af06dcde38033c21fdd7f8e96faa2fe0a1
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-02 14:00:54 +0100

    begue

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-function sendMail() {
-    var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
+
+
+  const button = document.getElementById("button");
+
+  button.addEventListener('click',function () {
+    // var params = {
+    //   const name = document.getElementById("name").value,
+    //   email: document.getElementById("email").value,
+    //   message: document.getElementById("message").value,
+    // };
   
-    const serviceID = "YOUR_SERVICE_ID";
-    const templateID = "YOUR_TEMPLETE_ID";
+    const serviceID = "service_7jbo24h";
+    const templateID = "template_nig6ruo";
+
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
   
-      emailjs.send(serviceID, templateID, params)
+      emailjs.send(serviceID, templateID, )
       .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
-  
+        if (name.value === "" || email.value === "" || message.value === "") {
+          alert("voleur c'est qui Codeloccol")
+        }else{
+          alert("Your message sent successfully!!");
+          name.value = "";
+          message.value = "";
+          email.value = "";
+        }
+          // document.getElementById("name").value = "";
+          // document.getElementById("email").value = "";
+          // document.getElementById("message").value = "";
+          // console.log(res);
+          // alert("Your message sent successfully!!");
       })
       .catch(err=>console.log(err));
-  
-  }
+  });
