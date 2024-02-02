975f28580b53b80660c18ee182a4a8bc4a270d8c
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-02 11:47:13 +0100

    add code html and js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    emailjs.sendForm('service_7jbo24h', 'template_nig6ruo', this)
-      .then(function(response) {
-        document.getElementById('status').innerHTML = 'Message sent successfully!';
-        document.getElementById('contact-form').reset();
-      }, function(error) {
-        document.getElementById('status').innerHTML = 'Error sending message: ' + error;
-      });
-  });
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
   
+    const serviceID = "YOUR_SERVICE_ID";
+    const templateID = "YOUR_TEMPLETE_ID";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
